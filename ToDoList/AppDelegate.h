f84ed987646d156c96c74991e44d9dfaffa2c706
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-05-30 11:41:26 -0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Daniel Kwiatkowski on 2015-05-30.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Daniel Kwiatkowski on 2015-05-30.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
